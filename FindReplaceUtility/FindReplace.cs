064c033212dd0db3feb9b724c41ac89b5f27d30f
juner <juner@juner.net>
2020-08-02 16:56:16 +0900

    おおまかな実装まで。 ただし、まだうまく動いていない。調査中。

---
+++ FindReplaceUtility/FindReplace.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using LazyRegex = System.Lazy<System.Text.RegularExpressions.Regex>;
+
+namespace FindReplaceUtility
+{
+    public class FindReplace
+    {
+        readonly LazyRegex include;
+        readonly LazyRegex exclude;
+        readonly LazyRegex find;
+        public string Path { get; }
+        public Regex Include => include.Value;
+        public Regex Exclude => exclude.Value;
+        public Regex Find => find.Value;
+        public string Replace { get; }
+        public void Deconstruct(out string Path, out Regex Include, out Regex Exclude, out Regex Find, out string Replace)
+            => (Path, Include, Exclude, Find, Replace)
+            = (this.Path, this.Include, this.Exclude, this.Find, this.Replace);
+        private LazyRegex ToLazyRegex(string Pattern) => new LazyRegex(() => new Regex(Pattern));
+        private LazyRegex ToLazyRegex(Regex Regex) => new LazyRegex(Regex);
+        public FindReplace(string Path, string Include, string Exclude, string Find, string Replace)
+            => (this.Path, include, exclude, find, this.Replace)
+            = (Path, ToLazyRegex(Include), ToLazyRegex(Exclude), ToLazyRegex(Find), Replace);
+        public FindReplace(string Path, Regex Include, Regex Exclude, Regex Find, string Replace)
+            => (this.Path, include, exclude, find, this.Replace)
+            = (Path, ToLazyRegex(Include), ToLazyRegex(Exclude), ToLazyRegex(Find), Replace);
+        public IEnumerable<FileInfo> Files()
+        {
+            return new DirectoryInfo(Path).EnumerateFiles(".*", new EnumerationOptions
+            {
+                AttributesToSkip = 0,
+                MatchType = MatchType.Simple,
+                IgnoreInaccessible = true,
+                RecurseSubdirectories = true,
+                MatchCasing = MatchCasing.PlatformDefault,
+            }).Where(DoMatching);
+        }
+        public bool DoMatching(FileInfo File) => Include.IsMatch(File.FullName) && !Exclude.IsMatch(File.FullName);
+        public bool DoMatching(string Path) => Include.IsMatch(Path) && !Exclude.IsMatch(Path);
+        public async ValueTask<bool> FindAndReplaceAsync(string Path, bool IsWrite = true, CancellationToken Token = default)
+        {
+            var Input = await File.ReadAllTextAsync(Path, Token);
+            var Output = Find.Replace(Input, Replace);
+            if (Input == Output)
+                return false;
+            if (IsWrite)
+                await File.WriteAllTextAsync(Path, Output, Token);
+            return true;
+        }
+    }
+}
---
+++ FindReplaceUtility/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace FindReplaceUtility
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            using var CancellationTokenSource = new CancellationTokenSource();
+            void CancelKeyPress(object sender, ConsoleCancelEventArgs args)
+            {
+                if (args.Cancel)
+                    CancellationTokenSource.Cancel();
+            }
+            Console.CancelKeyPress += CancelKeyPress;
+            var result = new Parser(settings =>
+            {
+                settings.IgnoreUnknownArguments = false;
+            }).ParseArguments<Options>(args);
+            return await result.MapResult(
+                option => Excute(option, CancellationTokenSource.Token), _ => Errors(result));
+        }
+        public static Task<int> Excute(Options Options, CancellationToken Token = default)
+        {
+            Console.WriteLine("Hello!");
+            var (Current, IsList, Include, Exclude, Find, Replace) = Options;
+            Console.WriteLine($"{nameof(Current)}:{Current}");
+            Console.WriteLine($"{nameof(IsList)}:{IsList}");
+            Console.WriteLine($"{nameof(Include)}:{Include}");
+            Console.WriteLine($"{nameof(Exclude)}:{Exclude}");
+            Console.WriteLine($"{nameof(Find)}:{Find}");
+            Console.WriteLine($"{nameof(Replace)}:{Replace}");
+            var fr = new FindReplace(Current, Include, Exclude, Find, Replace);
+            return Excute();
+            async Task<int> Excute()
+            {
+                try
+                {
+                    foreach (var FileInfo in fr.Files())
+                        try
+                        {
+                            Console.WriteLine(FileInfo);
+                            var result = await fr.FindAndReplaceAsync(FileInfo.FullName, !IsList, Token);
+                            if (result)
+                                Console.WriteLine("! " + FileInfo);
+                        }
+                        catch (Exception e2)
+                        {
+                            Console.WriteLine(e2);
+                        }
+                    return 0;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return 2;
+                }
+            }
+
+        }
+        public static Task<int> Errors(ParserResult<Options> Result)
+        {
+            var HelpText = CommandLine.Text.HelpText.AutoBuild(Result);
+            Console.WriteLine(HelpText);
+            return Task.FromResult(1);
+        }
+
+    }
+}
